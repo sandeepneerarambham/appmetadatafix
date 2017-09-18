c029cecaba17c24db71068f7948e541ac2d350c2
Toon De Coninck <toon@auth0.com>
2017-09-18 18:23:17 +0200

    inital version

---
+++ README.md
@@ -0,0 +1,10 @@
+# Update App Metadata
+
+This application is fetching the users and updating there `app_metadata`. This tool runs all operations serially, to avoid hitting the rate limits.
+
+## How install and Run
+
+- Install nodejs (v6.11.3) https://nodejs.org/en/download/
+- Run the command `npm i` in the root of the project where the `package.json` is located.
+- Rename the `.env.sample` to `.env` and set the all the values.
+- Run the command `node index.js`, and the process will start.
---
+++ index.js
@@ -0,0 +1,65 @@
+const request = require('superagent-bluebird-promise');
+const _ = require('lodash');
+const Promise = require('bluebird');
+require('dotenv').config();
+
+let accessToken;
+let usersUpdated = 0;
+
+function processBatch(page) {
+  const users_per_page = 100;
+  return request
+    .get(`${process.env.AUTH0_TENANT}/api/v2/users`)
+    .set('Authorization', 'Bearer ' + accessToken)
+    .query({
+      per_page: users_per_page,
+      include_totals: true,
+      fields: 'email,app_metadata,user_id',
+      page: page
+    })
+    .then(res => {
+      page++;
+
+      const users = _.filter(res.body.users, (user) => {
+        return _.has(user, 'app_metadata');
+      });
+
+      console.log(`Fetched next batch of users, found ${users.length} users with app_metadata.`);
+
+      return Promise.each(users, (user, index, length) => {
+        return request
+          .patch(`${process.env.AUTH0_TENANT}/api/v2/users/${encodeURIComponent(user.user_id)}`)
+          .set('Authorization', 'Bearer ' + accessToken)
+          .send({
+            'app_metadata': user.app_metadata
+          }).then(res => {
+            console.log(`[${++usersUpdated}] Synced app_metadata for ${user.user_id}`)
+            return res;
+          });
+      }).then((results) => {
+        if (users_per_page * page < res.body.total) {
+          return processBatch(page);
+        } else {
+          console.log('DONE');
+        }
+      });
+    });
+}
+
+request
+  .post(`${process.env.AUTH0_TENANT}/oauth/token`)
+  .send({
+    grant_type: 'client_credentials',
+    client_id: process.env.AUTH0_MGMTAPI_CLIENTID,
+    client_secret: process.env.AUTH0_MGMTAPI_CLIENTSECRET,
+    audience: `${process.env.AUTH0_TENANT}/api/v2/`
+  }).then(res => {
+    accessToken = res.body.access_token;
+    if (!accessToken) {
+      throw new Error('Unable to obtain access token');
+    }
+    return processBatch(0);
+  })
+  .catch(err => {
+    console.log(err.body);
+  });
